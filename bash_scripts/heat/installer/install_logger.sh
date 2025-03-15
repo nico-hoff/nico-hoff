e07588efb56c27d1680cd3ad4b00cb16bee246c0
nico-hoff <nico.hoff@web.de>
2025-03-15 20:12:17 +0100

    Update .gitignore, remove unused graph images, and modify logging scripts for dynamic paths

--- bash_scripts/heat/installer/install_logger.sh
+++ bash_scripts/heat/installer/install_logger.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Get the current working directory
+cwd=$(pwd)
+relativ_path="temp_logger.sh"
+
+ExecStart="$cwd/$relativ_path"
+
+cwd=$(pwd)
+LOGFILE="$cwd/data/raw/temp_log_multi.csv"
+
+echo "Logging to: $LOGFILE"
+
+# Get the current user
+current_user=$(whoami)
+
 # Create a systemd service file for temp_logger
 cat <<EOL | sudo tee /etc/systemd/system/temp_logger.service
 [Unit]
@@ -7,9 +21,9 @@ Description=Temperature Logger Service
 After=network.target
 
 [Service]
-ExecStart=/home/pi/Desktop/nico-hoff/bash_scripts/heat/temp_logger.sh
+ExecStart=$ExecStart
 Restart=always
-User=pi
+User=$current_user
 
 [Install]
 WantedBy=multi-user.target
@@ -24,4 +38,7 @@ sudo systemctl enable temp_logger.service
 # Start the service immediately
 sudo systemctl start temp_logger.service
 
-echo "temp_logger service has been added and started."
+echo "\ntemp_logger service has been added and started.\n"
+
+# Watch the logs of the temp_logger service
+sudo journalctl -u temp_logger.service -f
